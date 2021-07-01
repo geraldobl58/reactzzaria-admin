3068bafa29acb76daf4597670f58e3da773fb536
Geraldo Luiz <geraldobl58@gmail.com>
2021-07-01 12:03:26 -0300

    :sparkles: Select menu item as per current page

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,5 +1,5 @@
 import React, { lazy, Suspense } from 'react';
-import { Link, Switch, Route } from 'react-router-dom';
+import { Link, Switch, Route, useLocation } from 'react-router-dom';
 
 import {
   Typography,
@@ -17,44 +17,49 @@ const Orders = lazy(() => import('pages/Orders'))
 const PizzasSizes = lazy(() => import ('pages/PizzasSizes'));
 const PizzasFlavours = lazy(() => import('pages/PizzasFlavours'));
 
-const Main = () => (
-  <React.Fragment>
-    <MaterialDrawer variant='permanent'>
-      <DrawerContent>
-        <Typography variant='h4'>
-          Reactzzaria
-        </Typography>
-        <Typography>
-          (Sistema Administrativo)
-        </Typography>
-      </DrawerContent>
-      <Divider />
-      <List>
-        {menuItems.map(item => (
-          <ListItem
-            key={item.label}
-            button
-            component={Link}
-            to={item.link}
-          >
-          <ListItemText>{item.label}</ListItemText>
-        </ListItem>
-        ))}
-      </List>
-    </MaterialDrawer>
-    <Content>
-      <Suspense fallback='Carregando...'>
-        <Switch>
+const Main = () => {
+  const { pathname } = useLocation();
+
+  return (
+    <React.Fragment>
+      <MaterialDrawer variant='permanent'>
+        <DrawerContent>
+          <Typography variant='h4'>
+            Reactzzaria
+          </Typography>
+          <Typography>
+            (Sistema Administrativo)
+          </Typography>
+        </DrawerContent>
+        <Divider />
+        <List>
           {menuItems.map(item => (
-            <Route key={item.link} path={item.link} exact={item.exact}>
-              <item.component />
-            </Route>
+            <ListItem
+              key={item.label}
+              button
+              selected={pathname === item.link}
+              component={Link}
+              to={item.link}
+            >
+            <ListItemText>{item.label}</ListItemText>
+          </ListItem>
           ))}
-        </Switch>
-      </Suspense>
-    </Content>
-  </React.Fragment>
-)
+        </List>
+      </MaterialDrawer>
+      <Content>
+        <Suspense fallback='Carregando...'>
+          <Switch>
+            {menuItems.map(item => (
+              <Route key={item.link} path={item.link} exact={item.exact}>
+                <item.component />
+              </Route>
+            ))}
+          </Switch>
+        </Suspense>
+      </Content>
+    </React.Fragment>
+  )
+}
 
 const menuItems = [
   {
