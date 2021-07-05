74e387b2620cb2dac2f882388cef5817aadf3742
Geraldo Luiz <geraldobl58@gmail.com>
2021-07-05 16:21:51 -0300

    :sparkles: Pizza Sizes Page - Sizes List

--- src/pages/PizzasSizes/index.jsx
+++ src/pages/PizzasSizes/index.jsx
@@ -1,7 +1,57 @@
 import React from 'react';
 
+import {
+  Table,
+  TableRow,
+  TableCell,
+  TableBody
+} from '@material-ui/core';
+
+import {
+  MaterialTableContainer,
+  TableTitle,
+  THead,
+  Th
+} from './styles';
+
+import { useCollection } from 'hooks';
+
+import singularOrPlural from 'utils/singularOrPlural';
+
 function PizzasSizes() {
-  return <h1>PizzasSizes Component</h1>
+  const pizzasSizes = useCollection('sizes');
+  console.log(pizzasSizes);
+
+  return (
+    <MaterialTableContainer>
+      <TableTitle>
+        Tamanhos cadastrados
+      </TableTitle>
+      <Table>
+        <THead>
+          <TableRow>
+            <Th>Nome</Th>
+            <Th>Diâmetro</Th>
+            <Th>Fatias</Th>
+            <Th>Sabores</Th>
+          </TableRow>
+        </THead>
+        <TableBody>
+          {pizzasSizes?.map(pizza => (
+            <TableRow key={pizza.id}>
+              <TableCell>{pizza.name}</TableCell>
+              <TableCell>{pizza.size}</TableCell>
+              <TableCell>{pizza.slices}</TableCell>
+              <TableCell>
+                {pizza.flavours}{' '}
+                {singularOrPlural(pizza.flavours, 'sabor', 'sabores')}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </MaterialTableContainer>
+  )
 }
 
 export default PizzasSizes;
